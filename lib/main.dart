4bb8561eaa0c5e91b001c799c84510126073feed
Matheus <matheus.physicist369@gmail.com>
2024-05-16 11:48:52 -0300

    sksk

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_verbs/Principal.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  debugShowCheckedModeBanner: false,
+  home: Principal(),
+));
+
